200f8b29d3e6276168b44ff4aa49cb08b5966014
parnia <parniamaroofi@gmail.com>
2023-12-07 12:35:13 +0330

    Add router base

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,17 @@
 export default {
-  // mode: "spa",
-  // ssr: false,
-  // server: {
-  //   host: "0.0.0.0",
-  // },
+  mode: "spa",
+  ssr: false,
+  server: {
+    host: "0.0.0.0",
+  },
   /*
    ** Headers of the page
    */
+  router: {
+    base: "/assistant-demo/", // adjust to your repository name
+  },
   head: {
-    title: "assistant",
+    title: process.env.npm_package_name || "",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
